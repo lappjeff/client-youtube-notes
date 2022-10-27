713b9de850afbd04ed45f3fd3dfafc94ec80bc93
Jeff Lapp <lappjeff20@gmail.com>
2022-10-26 21:41:41 -0400

    wip: create dark mode for bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-
+      <button className='btn btn-primary'>Test</button>
     </div>
   );
 }
