27c9ec6f13775238a148da9bf38ed1cee978c854
Jeff Lapp <lappjeff20@gmail.com>
2022-10-27 11:55:22 -0400

    formatting

--- src/notes/Notes.tsx
+++ src/notes/Notes.tsx
@@ -1,41 +1,40 @@
-import { useEffect, useState } from 'react';
-import Note from './note/Note';
-
+import { useEffect, useState } from "react";
+import Note from "./note/Note";
 
 function Notes() {
-    const [notes, setNotes] = useState([])
+  const [notes, setNotes] = useState([]);
 
-    useEffect(() => {
-        
-        function handleNotes() {
-        const localNotes = localStorage.getItem('yt-notes')
-  
-        if(!localNotes) return 
-  
-        setNotes(Object.values(JSON.parse(localNotes)))
-  
-      }
-  
-      window.addEventListener('storage', handleNotes)
-  
-      // handles cleanup
-      return () => window.removeEventListener('storage', handleNotes)
-    }, [])
+  useEffect(() => {
+    function handleNotes() {
+      const localNotes = localStorage.getItem("yt-notes");
 
-    return <>
-    {notes.map((notesArray: any[]) => {
+      if (!localNotes) return;
+
+      setNotes(Object.values(JSON.parse(localNotes)));
+    }
+
+    window.addEventListener("storage", handleNotes);
+
+    // handles cleanup
+    return () => window.removeEventListener("storage", handleNotes);
+  }, []);
+
+  return (
+    <>
+      {notes.map((notesArray: any[]) => {
         return (
-        <>
-            <h2>
-                {notesArray[0].id}
-            </h2>
-        <>
-            {notesArray.map(note => <Note note={note}/>)}
-        </>
-
-        </>
-        )
-    })}</>
+          <>
+            <h2>{notesArray[0].id}</h2>
+            <>
+              {notesArray.map((note) => (
+                <Note note={note} />
+              ))}
+            </>
+          </>
+        );
+      })}
+    </>
+  );
 }
 
-export default Notes
+export default Notes;
